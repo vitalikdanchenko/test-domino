068afd2fc5e27f2a49a13e1da498e8265b06d89f
vitalikdanchenko <devyjones@gmail.com>
2016-10-03 18:23:34 +0300

    Initial Commit

---
+++ test-domino/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-domino
+//
+//  Created by Vitalik Danchenko on 03.10.16.
+//  Copyright © 2016 Talkable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test-domino/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test-domino
+//
+//  Created by Vitalik Danchenko on 03.10.16.
+//  Copyright © 2016 Talkable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
